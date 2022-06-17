5a2c38905170abc0f53c52ae16028e4e7b52e5ab
luka <l.lortkifanidze@noxtton.com>
2022-06-17 23:34:27 +0400

    bsearch and seq-search

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+
+
+def binary_search(lst,target):
+
+    L = 0
+    R = len(lst)-1
+
+    while (L <= R):
+        M = (R+L)//2
+        if lst[M] == target:
+            return M
+        elif lst[M] > target:
+            R = M - 1
+        else: #lst[M] < target
+            L = M + 1
+
+    return -1
+
+
+test_list = [1,2,3,4,5,21414,12121,422,2]
+target = 5
+
+assert binary_search([1,2,3,4,5,6],1) == 0
+assert binary_search([1,2,3,4,5,6],2) == 1
+assert binary_search([1,2,3,4,5,6],3) == 2
+assert binary_search([1,2,3,4,5,6],4) == 3
+assert binary_search([1,2,3,4,5,6],5) == 4
+assert binary_search([1,2,3,4,5,6],6) == 5
+assert binary_search([1,2,3,4,5,6],7) == -1
---
+++ sequential_search.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int sequential_sort(int array[],int arrSize, int target){
+
+	int i;
+	for (i = 0; i < arrSize; i++){
+		if (array[i] == target){
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int main(){
+
+	int arraySize;
+	printf("Enter array size:");
+	scanf("%d",&arraySize);
+
+	int array[arraySize];
+	int i;
+	for (i = 0;i < arraySize;i++){
+		printf("Enter %d element of the array: \n",i);
+		scanf("%d",&array[i]);
+	}
+	
+	int target;
+	printf("Enter the target number: ");
+	scanf("%d",&target);
+
+	int search_result = sequential_sort(array,arraySize,target);
+	if (search_result != -1){
+		printf("index of element %d is: %d\n",target, search_result); 
+	}else{
+		printf("target not found\n");
+	}
+
+	return 0;
+}
